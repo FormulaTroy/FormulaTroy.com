596137c8aa3a6d4d9495d6ceed78e47d677dc40e
FormulaTroy <7297461+FormulaTroy@users.noreply.github.com>
2023-02-08 23:11:20 -0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Under Maintenance</title>
+    <style>
+      body {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 100vh;
+        background-color: lavender;
+        font-family: Arial, sans-serif;
+      }
+
+      h1 {
+        font-size: 36px;
+        color: #333;
+        text-align: center;
+        margin-top: -100px;
+      }
+    </style>
+  </head>
+  <body>
+    <h1>Site is currently under maintenance</h1>
+  </body>
+</html>
