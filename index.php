0f9667c0aaf77644d31b9fb780ba10468cde2e69
FormulaTroy <7297461+FormulaTroy@users.noreply.github.com>
2023-02-11 19:46:57 -0800

    Bootstrap 5.3 + Icons 1.10

--- index.php
+++ index.php
@@ -6,7 +6,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>FormulaTroy</title>
   <link href="/css/bootstrap.min.css" rel="stylesheet">
+  <link href="/css/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
 </head>
+
 <body>
   <header>
     <nav class="navbar navbar-expand-md navbar-dark bg-dark" aria-label="Fourth navbar example">
@@ -17,7 +19,7 @@
           <span class="navbar-toggler-icon"></span>
         </button>
 
-        <div class="collapse navbar-collapse" id="navbarsExample04">
+        <div class="collapse navbar-collapse">
           <ul class="navbar-nav me-auto mb-2 mb-md-0">
             <li class="nav-item">
               <a class="nav-link active" aria-current="page" href="#">Home</a>
@@ -29,9 +31,9 @@
               <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
             </li>
             <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="dropdown04" data-bs-toggle="dropdown"
-                aria-expanded="false">Dropdown</a>
-              <ul class="dropdown-menu" aria-labelledby="dropdown04">
+              <a class="nav-link dropdown-toggle" href="#" id="dropdown-apps" data-bs-toggle="dropdown"
+                aria-expanded="false">Web Apps</a>
+              <ul class="dropdown-menu" aria-labelledby="dropdown-apps">
                 <li><a class="dropdown-item" href="#">Action</a></li>
                 <li><a class="dropdown-item" href="#">Another action</a></li>
                 <li><a class="dropdown-item" href="#">Something else here</a></li>
@@ -39,15 +41,11 @@
             </li>
           </ul>
         </div>
-      </div><!-- end container -->
+
+      </div>
     </nav>
   </header>
-
   <main>
-
-
-
-
 
     <section class="py-3 text-center container">
       <div class="row py-lg-3">
@@ -64,80 +62,43 @@
     </section>
 
 
-      <div class="container">
+    <div class="container">
 
-        <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-3">
-          <div class="col">
-            <div class="card shadow-sm">
-              <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg"
-                role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
-                <title>Placeholder</title>
-                <rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef"
-                  dy=".3em">Thumbnail</text>
-              </svg>
+      <div class="row row-cols-1 row-cols-md-2 row-cols-xl-3 g-3">
+        <div class="col">
+          <div class="card shadow-sm">
+            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg"
+              role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
+              <title>Placeholder</title>
+              <rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef"
+                dy=".3em">Thumbnail</text>
+            </svg>
 
-              <div class="card-body">
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
-                  content. This content is a little bit longer.</p>
-                <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group">
-                    <button type="button" class="btn btn-sm btn-primary">View</button>
-                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                  </div>
-                  <small class="text-muted">9 mins</small>
+            <div class="card-body">
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
+                content. This content is a little bit longer.</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-primary">Launch Project Page</button>
+                  <button type="button" class="btn btn-sm btn-outline-secondary"><i class="bi-github"></i> Source Code</button>
                 </div>
+                <small class="text-muted">Created 2015</small>
               </div>
             </div>
           </div>
-
-
-
-
-
-
-
-
-
-
         </div>
+
       </div>
-
-
-
-
+    </div>
 
   </main>
+  <footer class="mt-5">
+    <div class="container">
+      <p>Footer Text</p>
+    </div>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  </footer>
   <script src="/js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
