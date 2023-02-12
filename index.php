2875519c2d64f4a1d316419f325c66bb8c70cbc7
FormulaTroy <7297461+FormulaTroy@users.noreply.github.com>
2023-02-12 00:13:02 -0800

    Init Footer

--- index.php
+++ index.php
@@ -102,9 +102,9 @@
     </div>
 
   </main>
-  <footer class="mt-5">
+  <footer class="mt-5 pt-3">
     <div class="container">
-      <p>Footer Text</p>
+      <p>FormulaTroy.com<br>&copy; <?php echo date("Y"); ?></p>
     </div>
   </footer>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
