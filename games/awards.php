9c6754fefc2cc07576367e4a9346ebe60382acba
FormulaTroy <7297461+FormulaTroy@users.noreply.github.com>
2023-04-09 16:29:24 -0700

    Init gaming project

---
+++ games/awards.php
@@ -0,0 +1,33 @@
+<?php
+$pageTitle = "Google Calendar Dark Mode";
+include_once "../php-inc/ft-functions.php";
+include_once "../php-inc/ft-header.php";
+?>
+
+<main>
+
+  <?php
+  # Hero
+  CreateHeroText("Gaming Awards", "The best games I've played each year.");
+  ?>
+
+  <?php # Page ?>
+  <div class="container">
+    <div class="row g-3">
+      <div class="col-12">
+
+
+
+        <p>Datatable goes here</p>
+
+
+
+
+
+      </div>
+    </div>
+  </div>
+
+</main>
+
+<?php include_once "../php-inc/ft-footer.php"; ?>
--- php-inc/ft-header.php
+++ php-inc/ft-header.php
@@ -11,11 +11,16 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>FormulaTroy <?php if ($pageTitle) {echo "| $pageTitle";}?></title>
+  <title>FormulaTroy
+    <?php if ($pageTitle) {
+      echo "| $pageTitle";
+    } ?>
+  </title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
-  <link rel="stylesheet" href="css/formulatroy.css">
+  <link rel="stylesheet" href="/css/formulatroy.css">
+  <link rel="shortcut icon" type="image/x-icon" href="/img/favicon.ico"/>
 </head>
 
 <body>
@@ -34,6 +39,16 @@
               <a class="nav-link px-md-4" aria-current="page" href="/"><i class="bi bi-house-heart"></i> Index</a>
             </li>
             <li class="nav-item dropdown">
+              <a class="nav-link px-md-4 dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <i class="bi bi-controller"></i> Games
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                <li><a class="dropdown-item" href="/games/played"><i class="bi bi-journal-check"></i> Played</a></li>
+                <li><a class="dropdown-item" href="/games/backlog"><i class="bi bi-database-add"></i> Backlog</a></li>
+                <li><a class="dropdown-item" href="/games/awards"><i class="bi bi-trophy"></i> Awards</a></li>
+              </ul>
+            </li>
+            <li class="nav-item dropdown">
               <a class="nav-link px-md-4 dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
                 data-bs-toggle="dropdown" aria-expanded="false">
                 <i class="bi bi-palette"></i> Themes
@@ -41,7 +56,8 @@
               <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
                 <li><a class="dropdown-item disabled" href="#"><i class="bi bi-discord"></i> Discord Theme (WIP)</a>
                 </li>
-                <li><a class="dropdown-item" href="/gc-dark"><i class="bi bi-calendar3"></i> Google Calendar Dark Mode</a></li>
+                <li><a class="dropdown-item" href="/gc-dark"><i class="bi bi-calendar3"></i> Google Calendar Dark
+                    Mode</a></li>
                 <li><a class="dropdown-item disabled" href="#"><i class="bi bi-steam"></i> Steam Deck Theme (WIP)</a>
                 </li>
               </ul>
@@ -54,8 +70,7 @@
               <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
                 <li><a class="dropdown-item" href="https://formulatroy.com/app/racing-calculator/" target="_blank"><i class="bi bi-fuel-pump"></i> Racing Fuel Calculator</a></li>
                 <li><a class="dropdown-item" href="https://formulatroy.com/app/chess/" target="_blank"><i class="bi bi-box-arrow-up-right"></i> Chessboard</a></li>
-                <li><a class="dropdown-item" href="https://formulatroy.com/app/canasta/" target="_blank"><i class="bi bi-suit-spade-fill"></i> Canasta Score Tracker</a>
-                </li>
+                <li><a class="dropdown-item" href="https://formulatroy.com/app/canasta/" target="_blank"><i class="bi bi-suit-spade-fill"></i> Canasta Score Tracker</a></li>
               </ul>
             </li>
           </ul>
