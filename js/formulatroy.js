e2a3153e6d7a46296ab6f48100667a7edcd0cbc8
FormulaTroy <7297461+FormulaTroy@users.noreply.github.com>
2023-12-03 01:27:12 -0800

    Cancel the Games display feature

--- js/formulatroy.js
+++ js/formulatroy.js
@@ -1,36 +1,3 @@
 $(document).ready(function() {
 
-  $('#gamesPlayedTable').DataTable({
-    "pageLength": 10,
-    "lengthMenu": [ [10, 25, 50, -1], [10, 25, 50, "All"] ],
-
-
-    //"order": [ 0, 'desc' ],
-    "order": [ 5, 'desc' ],
-
-
-    "columns": [
-      // Rating
-      { "width": "50px" },
-      // Art
-      { "width": "80px", orderable: false, searchable: false },
-      // Game Title
-      null,
-      // Platform
-      { "width": "150px" },
-      // Hours
-      { "width": "30px", searchable: false },
-      // Completed
-      { "width": "100px" }
-    ]
-  });
-
-
-
-
-
-
-
-
-
 });// end doc ready
