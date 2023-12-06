2c221ffeba9c4c0e1c7cdf8839ca2e150c7b275e
FormulaTroy <7297461+FormulaTroy@users.noreply.github.com>
2023-12-05 20:39:31 -0800

    Update ft-header.php

--- php-inc/ft-header.php
+++ php-inc/ft-header.php
@@ -11,11 +11,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>FormulaTroy
-    <?php if ($pageTitle) {
-      echo "| $pageTitle";
-    } ?>
-  </title>
+  <title><?php if ($pageTitle) {echo "$pageTitle | ";}?>FormulaTroy</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
