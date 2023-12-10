84ed49c9c00042ccb431bc2d555f36e5088e160d
FormulaTroy <7297461+FormulaTroy@users.noreply.github.com>
2023-12-10 15:58:07 -0800

    Add Google Cal Dark Mode Changelog

--- gc-dark.php
+++ gc-dark.php
@@ -43,6 +43,31 @@ include_once "php-inc/ft-header.php";
         <p><img src="img/projects/ftgcdm.png" class="img-fluid" alt=""></p>
         <p><a class="btn btn-outline-secondary" href="https://github.com/FormulaTroy/FormulaTroy.com/blob/main/theme/google-cal-dark/FTGoogleCalDarkMode.user.css" role="button" target="_blank"><i class="bi bi-github"></i> View Code on GitHub</a></p>
         <p class="fs-2"><i class="bi bi-filetype-css pe-3 ft-css-badge"></i></p>
+        <h4>Changelog</h4>
+        <div class="accordion" id="changelog">
+          <?php
+            LogChange(120, "v1.2.0 - December 2nd, 2023", "<ul>
+                <li>Clean up homepage UI discrepancies stemming from class name changes on Google's side.</li>
+                <li>Clean up Create/Edit Event screen discrepancies stemming from class name changes on Google's side.</li>
+                <li>Override icon colors to be forced into the same color states as the rest of their neighbors.</li>
+                <li>Changed box models of various Create/Edit Event screen UI elements to make them all align. This feels like a bug on Google's side with negative margins that may get fixed at some point.</li>
+                <li>Themed the Create/Edit Event screen dropdowns and in-form buttons.</li>
+                <li>Themed the Calendar view selector (week, month, etc).</li>
+              </ul>");
+            LogChange(114, "v1.1.4 - February 26th, 2023", "<ul>
+                <li>Fixed theming issues with the top left calendar and date picker.</li>
+                <li>Themed the collapsable right side panel.</li>
+                <li>Edit event title colors.</li>
+              </ul>");
+            LogChange(110, "v1.1.0 - February 10th, 2023", "<ul>
+                <li>First Release Candidate.</li>
+                <li>Init core variables and override Google variables.</li>
+                <li>Create global color and background-color override statements.</li>
+                <li>Automatic update URL hosted by GitHub Pages deployment.</li>
+                <li>Let there be darkness!</li>
+              </ul>");
+          ?>
+        </div>
       </div>
 
     </div>
