a03dbcbf640607b466425b4552899a88e9e5d090
FormulaTroy <7297461+FormulaTroy@users.noreply.github.com>
2023-12-12 20:31:12 -0800

    Update footer design

--- php-inc/ft-footer.php
+++ php-inc/ft-footer.php
@@ -1,14 +1,29 @@
-<footer class="mt-5 pt-3">
-  <div class="container">
-    <p>FormulaTroy.com<br>&copy;
-      <?php echo date("Y"); ?>
-    </p>
-  </div>
-</footer>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
-  integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-<script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
-<script src="/js/formulatroy.js"></script>
+  <footer class="mt-5 pt-3">
+    <div class="container">
+      <div class="row row-cols-1 row-cols-sm-3 g-3">
+        <div class="col text-center social-links">
+          <p>
+            <a href="https://www.backloggd.com/u/FormulaTroy/" target="_blank"><i class="bi bi-controller"></i> Backloggd</a><br>
+            <a href="https://www.youtube.com/FormulaTroy" target="_blank"><i class="bi bi-youtube"></i> YouTube</a><br>
+            <a href="https://github.com/FormulaTroy" target="_blank"><i class="bi bi-github"></i> GitHub</a>
+          </p>
+        </div>
+        <div class="col text-center">
+          <p class="fs-5">Made with <i class="bi bi-heart-fill"></i></p>
+        </div>
+        <div class="col text-center">
+          <p>
+            <span class="fs-5">FormulaTroy</span><br>
+            &copy; <?php echo date("Y"); ?>
+          </p>
+        </div>
+        <p>&nbsp;</p>
+      </div>
+    </div>
+  </footer>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
+  <script src="/js/formulatroy.js"></script>
 </body>
-
 </html>
