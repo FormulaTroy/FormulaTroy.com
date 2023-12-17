ae8229e040dec7fc8544f2813549e6b6fc6950d6
FormulaTroy <7297461+FormulaTroy@users.noreply.github.com>
2023-12-17 02:39:17 -0800

    Prepros minify

--- php-inc/ft-header.php
+++ php-inc/ft-header.php
@@ -9,7 +9,7 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
-  <link rel="stylesheet" href="/css/formulatroy.css">
+  <link rel="stylesheet" href="/css/formulatroy.min.css">
   <link rel="shortcut icon" type="image/x-icon" href="/img/favicon.ico"/>
 </head>
 
