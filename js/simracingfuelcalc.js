2393ee16ed205d6185c696058383f3d999a5a4cf
FormulaTroy <7297461+FormulaTroy@users.noreply.github.com>
2025-02-07 20:34:26 -0800

    Init Shorthand Input Calculator logic

--- js/simracingfuelcalc.js
+++ js/simracingfuelcalc.js
@@ -90,7 +90,7 @@ $(document).ready(function () {
   }
 
   // trigger: change calculator type
-  $('input[name="calcType"]').on("change", function() {
+  $('input[name="calcType"]').on("change", function () {
     let calcType = $('input[name="calcType"]:checked').val();
     swapCalcTypeDisplay(calcType);
     validateFormInputs();
@@ -107,10 +107,10 @@ $(document).ready(function () {
   }
 
   // trigger: update fields based on raceDistanceType
-  $("#raceDistanceType").on("change",function(){
+  $("#raceDistanceType").on("change", function () {
     let raceDistanceTypeValue = $(this).val();
     isLapTimeFieldNeeded();
-    $('#raceDistanceUnitsWrapper label[for="raceDistanceUnits"]').html("How Many "+raceDistanceTypeValue+" Is The Race?");
+    $('#raceDistanceUnitsWrapper label[for="raceDistanceUnits"]').html("How Many " + raceDistanceTypeValue + " Is The Race?");
   });
 
   // helper: convert lap time as string (1:32.932) into seconds (92.932)
@@ -139,7 +139,7 @@ $(document).ready(function () {
   //// then call the calculation function
 
   // trigger: call validation whenever any field changes or key is pressed
-  $("form").on("change keyup", ":input", function() {
+  $("form").on("change keyup", ":input", function () {
     validateFormInputs();
   });
 
@@ -165,7 +165,52 @@ $(document).ready(function () {
     switch (calcType.val()) {
       case "calcShorthand":
         shorthandInput = $("#shorthandInput");
-        // TO DO
+
+        if (shorthandInput.val().trim() === "") {
+          isError = 1;
+        }
+
+        // split the input into parts for each variable
+        const inputPartsArray = shorthandInput.val().split(" ");
+
+        if (inputPartsArray.length >= 4 && inputPartsArray.length < 6) {
+
+          // Check RaceMinutes (float), FuelPerLap (float), FuelTankSize (float)
+          if (isNaN(parseFloat(inputPartsArray[0])) || isNaN(parseFloat(inputPartsArray[2])) || isNaN(parseFloat(inputPartsArray[3]))) {
+            isError = 1;
+          }
+
+          // Check LapTime (float:float)
+          if (typeof inputPartsArray != "undefined") {
+            const lapTimeString = inputPartsArray[1].split(":");
+            if (lapTimeString.length !== 2 || isNaN(parseFloat(lapTimeString[0])) || isNaN(parseFloat(lapTimeString[1]))) {
+              isError = 1;
+            }
+          }
+
+          // check AdditionalLaps (optional, float)
+          // if it's there, check it's not just a trailing space, and that it's a float
+          if (typeof inputPartsArray[4] != "undefined" || typeof inputPartsArray[4] == "") {
+            if (isNaN(parseFloat(inputPartsArray[4]))) {
+              isError = 1;
+            }
+          } else if (typeof inputPartsArray[4] == "undefined") {
+            // if it's not there, set to 0
+            inputPartsArray[4] = 0;
+          }
+
+        } else {
+          isError = 1; // Not enough elements in array, or too many
+        }
+
+        if (isError == 1) {
+          shorthandInput.addClass("is-invalid");
+          results.append("<h5>Shorthand Input</h5><p>This text field requires very specific formatting in order to calculate results. Each variable needs a single space between them. But make sure there is not an extra space at the end of your input.</p><ul><li>RaceMinutes: How long the race is in minutes (i.e. 1.5hrs = 90)</li><li>LapTime: The average race lap time formatted as MM:SS:MS</li><li>FuelPerLap: The average fuel used per lap</li><li>FuelTankSize: The maximum fuel tank capacity</li><li>AdditionalLaps (optional): Extra laps of fuel buffer, usually 1</li></ul><p>Examples of valid inputs:</p><ul><li>144 1:30.432 3.2 90 1</li><li>90 0:59 3.35 100</li><li>30 1:49.5 4.2 150 1.5</li></ul>");
+        } else {
+          shorthandInput.removeClass("is-invalid");
+          calculateShorthandResults(parseFloat(inputPartsArray[0]), inputPartsArray[1], parseFloat(inputPartsArray[2]), parseFloat(inputPartsArray[3]), parseFloat(inputPartsArray[4]));
+        }
+
         break;
 
       case "calcSprint":
@@ -255,21 +300,19 @@ $(document).ready(function () {
         strategyText = "Pit Stop for ";
       }
 
-      // TO DO: MAKE ITERATOR AND SET VALUE TO 0, then call it back and set value correctly
-
       // if remaining fuel >= a full tank, add full stop, otherwise add final partial stop
       if (finalFuelNeeded >= raceFuelTankSize) {
-        results.append('<div class="progress" role="progressbar"><div class="progress-bar overflow-visible bg-success text-start" style="width: 100%">&nbsp;&nbsp;&nbsp;'+strategyText+raceFuelTankSize+' (100%)</div></div>');
-      } else if (finalFuelNeeded >= (.20*raceFuelTankSize) ) {
+        results.append('<div class="progress" role="progressbar"><div class="progress-bar overflow-visible bg-success text-start" style="width: 100%">&nbsp;&nbsp;&nbsp;' + strategyText + raceFuelTankSize + '</div></div>');
+      } else if (finalFuelNeeded >= (.20 * raceFuelTankSize)) {
         stintFractionToPercent = finalFuelNeeded / raceFuelTankSize * 100;
         stintFractionToPercent = parseFloat(stintFractionToPercent.toFixed(2));
         finalFuelNeeded = parseFloat(finalFuelNeeded.toFixed(2));
-        results.append('<div class="progress" role="progressbar"><div class="progress-bar overflow-visible progress-bar-striped progress-bar-animated bg-primary text-start" style="width: '+stintFractionToPercent+'%">&nbsp;&nbsp;&nbsp;'+strategyText+finalFuelNeeded+' ('+stintFractionToPercent+'%)</div></div>');
+        results.append('<div class="progress" role="progressbar"><div class="progress-bar overflow-visible progress-bar-striped progress-bar-animated bg-primary text-start" style="width: ' + stintFractionToPercent + '%">&nbsp;&nbsp;&nbsp;' + strategyText + finalFuelNeeded + '</div></div>');
       } else {
         stintFractionToPercent = finalFuelNeeded / raceFuelTankSize * 100;
         stintFractionToPercent = parseFloat(stintFractionToPercent.toFixed(2));
         finalFuelNeeded = parseFloat(finalFuelNeeded.toFixed(2));
-        results.append('<div class="progress" role="progressbar"><div class="progress-bar overflow-visible progress-bar-striped progress-bar-animated bg-danger text-start" style="width: '+stintFractionToPercent+'%">&nbsp;&nbsp;&nbsp;'+strategyText+finalFuelNeeded+' ('+stintFractionToPercent+'%)</div></div>');
+        results.append('<div class="progress" role="progressbar"><div class="progress-bar overflow-visible progress-bar-striped progress-bar-animated bg-danger text-start" style="width: ' + stintFractionToPercent + '%">&nbsp;&nbsp;&nbsp;' + strategyText + finalFuelNeeded + ' - Try Fuel Saving Instead?</div></div>');
       }
 
       finalFuelNeeded -= raceFuelTankSize;
@@ -277,9 +320,32 @@ $(document).ready(function () {
   }
 
   // helper: calc results for shorthand input
-  function calculateShorthandResults() {
-    // get values directly, versus getting the field like in validation function
-    let shorthandInput = $("#shorthandInput").val();
+  function calculateShorthandResults(raceMinutes, raceLapTimeRawValue, raceFuelPerLap, raceFuelTankSize, raceFuelBufferLaps) {
+    // convert lap time
+    let raceLapTimeCleaned = raceLapTimeRawValue.replace(/[^0-9:.]/g, '');
+    let raceLapTimeInSeconds = convertLapTimeStringToSeconds(raceLapTimeCleaned);
+    // init final results vars
+    let results = $("#results");
+    results.html("");
+    let finalLaps = 0;
+    let finalLapsWithBuffer = 0;
+    let finalFuelNeeded = 0;
+    let finalStintsNeeded = 0;
+
+    // strategy calc
+    finalLaps = Math.ceil(raceMinutes * 60 / raceLapTimeInSeconds);
+    finalLapsWithBuffer = parseFloat(finalLaps) + parseFloat(raceFuelBufferLaps);
+    finalFuelNeeded = finalLapsWithBuffer * raceFuelPerLap; // total fuel
+    finalFuelNeeded = parseFloat(finalFuelNeeded.toFixed(2));
+    finalStintsNeeded = finalFuelNeeded / raceFuelTankSize // number of stints
+    finalStintsNeeded = parseFloat(finalStintsNeeded.toFixed(2));
+
+    // display results
+    results.append("<p>Total Race Laps: <strong>" + finalLaps + "</strong></p>");
+    results.append("<p>Total Fuel: <strong>" + finalFuelNeeded + "</strong></p>");
+    results.append("<p>Total Stints: <strong>" + finalStintsNeeded + "</strong></p>");
+    stintsToProgressBars(finalFuelNeeded, raceFuelTankSize, results);
+
   }
 
   // helper: calc results for sprint races
@@ -314,7 +380,7 @@ $(document).ready(function () {
     }
 
     // protect extra lap fuel buffer from being null
-    if (!raceFuelBufferLaps) {raceFuelBufferLaps = 0}
+    if (!raceFuelBufferLaps) { raceFuelBufferLaps = 0 }
 
     // calc final strategy numbers
     finalLapsWithBuffer = parseFloat(finalLaps) + parseFloat(raceFuelBufferLaps) + parseFloat(racePaceLapFuel); // add buffers
@@ -324,9 +390,9 @@ $(document).ready(function () {
     finalStintsNeeded = parseFloat(finalStintsNeeded.toFixed(2));
 
     // display results
-    results.append("<p>Total Race Laps: <strong>"+finalLaps+"</strong></p>");
-    results.append("<p>Total Fuel: <strong>"+finalFuelNeeded+"</strong></p>");
-    results.append("<p>Total Stints: <strong>"+finalStintsNeeded+"</strong></p>");
+    results.append("<p>Total Race Laps: <strong>" + finalLaps + "</strong></p>");
+    results.append("<p>Total Fuel: <strong>" + finalFuelNeeded + "</strong></p>");
+    results.append("<p>Total Stints: <strong>" + finalStintsNeeded + "</strong></p>");
     stintsToProgressBars(finalFuelNeeded, raceFuelTankSize, results);
   }
 
