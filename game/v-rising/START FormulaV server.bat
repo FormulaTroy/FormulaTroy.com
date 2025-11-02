c0b26d37b1043d610495274696c6204e5da8f325
Troy Ravencroft <7297461+FormulaTroy@users.noreply.github.com>
2025-11-01 21:26:47 -0700

    V Rising Server Config Backups

---
+++ game/v-rising/START FormulaV server.bat
@@ -0,0 +1,15 @@
+@echo off
+REM Copy this script to your own file and modify to your content. This file can be overwritten when updating.
+set SteamAppId=1604030
+echo "Starting V Rising Dedicated Server - PRESS CTRL-C to exit"
+
+@echo on
+VRisingServer.exe
+-persistentDataPath .\save-data
+-serverName "FormulaV"
+-saveName "formulav"
+-logFile ".\logs\formulavServer.log"
+-hideIpAddress true
+-lowerFPSWhenEmpty true
+-lowerFPSWhenEmptyValue 5
+-listOnEOS true
